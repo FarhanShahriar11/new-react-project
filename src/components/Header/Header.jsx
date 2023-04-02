e5dac59bc98a198ae8c0d31ae02fb699a1885b46
FarhanShahriar11 <farhanshahriar014@gmail.com>
2023-04-03 02:28:36 +0600

    20

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,14 @@ const Header = () => {
                 <Navbar.Collapse className="justify-content-end">
                    
                 </Navbar.Collapse>
+                <ul>
+            <li><a href="#home">Home</a></li>
+             <li><a href="#news">News</a></li>
+             <li><a href="#contact">Contact</a></li>
+              <li><a href="#about">About</a></li>
+              </ul>
             </Container>
+           
         </Navbar>
     );
 };
